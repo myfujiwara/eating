7df1dc3aeab78ea170eaa70340e72061a608fb54
myfujiwara <my22wara18@gmail.com>
2020-10-26 09:23:44 +0000

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  
+  def new
+  end
+
+  def create
+  end
+
+  def sign_in
+  end
+
+  def sign_in_process
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def bookmark
+  end
+
+  def message
+  end
+
+  def application
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :posts
+  get '/sign_in', to:'users#sign_in', as: :sign_in
+  post '/sign_in', to: 'users#sign_in_process'
+  get '/', to: 'posts#index', as: :top
+  get '/users/bookmark', to: 'users#bookmark', as: :bookmark
+  get '/message', to: 'users#message', as: :message
+  get '/users/application', to: 'users#application'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
